c4e366dc34dfb4e36f55d87d4c1189abdb206e27
Dublonx <dublonjmx17@gmail.com>
2026-03-26 21:32:53 +0800

    Personal Portfolio

---
+++ main.js
@@ -0,0 +1,14 @@
+// preview image
+const overlay = document.querySelector('#overlay');
+const overlayImg = document.querySelector('#overlay img');
+
+document.querySelectorAll('.cert-img').forEach(img => {
+    img.onclick = () => {
+        overlay.classList.add("active");
+        overlayImg.src = img.src;
+    };
+});
+
+overlay.onclick = () => {
+    overlay.classList.remove("active");
+};
