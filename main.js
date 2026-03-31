af20cd6b362d53693c88e4218936c86a441e9a8a
Dublonx <dublonjmx17@gmail.com>
2026-03-31 20:26:26 +0800

    Update and add dark mode

--- main.js
+++ main.js
@@ -14,4 +14,16 @@ overlay.onclick = () => {
 };
 
 // light/dark
+const toggleBtn = document.querySelector('#toggleBtn');
+const body = document.body;
 
+toggleBtn.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+
+    // change icon
+    if (body.classList.contains("dark-mode")) {
+        toggleBtn.className = "fa-solid fa-sun";
+    } else {
+        toggleBtn.className = "fa-solid fa-moon";
+    }
+})
